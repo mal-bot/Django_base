a94a57b2010401135231da97d782cb8a6e83a99e
em <moy_garb@bk.ru>
2020-07-30 13:15:59 +0300

    Lesson 3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render
 from json import load
+import os
+from geekshop.settings import BASE_DIR
+from .models import Product, ProductCategory
 
 
 def main(request):
-    with open(r'mainapp\contexts_lesson_2\main_context.json', 'r') as file:
-        context = load(file)
+    content_products = Product.objects.all()[:4]
+    context = {
+
+        'title': 'главная',
+        'main_menu': [
+                        {'href': 'main', 'name': 'домой'},
+                        {'href': 'products', 'name': 'продукты'},
+                        {'href': 'contacts', 'name': 'контакты'}
+                    ],
+        'content_products': content_products
+    }
+    # file_path = os.path.join(
+    #     BASE_DIR, 'mainapp/contexts_lesson_2/main_context.json')
+    # if os.path.exists(file_path):
+    #     with open(file_path) as file:
+    #         context = load(file)
     return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    with open(r'mainapp\contexts_lesson_2\products_context.json', 'r') as file:
-        context = load(file)
+    file_path = os.path.join(
+        BASE_DIR, 'mainapp/contexts_lesson_2/products_context.json')
+    if os.path.exists(file_path):
+        with open(file_path) as file:
+            context = load(file)
+
     return render(request, 'mainapp/products.html', context)
 
 
 def contacts(request):
-    with open(r'mainapp\contexts_lesson_2\contacts_context.json', 'r') as file:
-        context = load(file)
-    return render(request, 'mainapp/contact.html', context)
+    file_path = os.path.join(
+        BASE_DIR, 'mainapp/contexts_lesson_2/contacts_context.json')
+    if os.path.exists(file_path):
+        with open(file_path) as file:
+            context = load(file)
+        return render(request, 'mainapp/contact.html', context)
